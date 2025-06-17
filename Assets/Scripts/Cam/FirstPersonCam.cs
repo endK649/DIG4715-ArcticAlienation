27ef73910e18a69de32cc53539d217a09e6aac3a
Okami <32124flame@gmail.com>
2025-06-17 07:54:12 -0400

    Revert "Last Minute Camera Update"
    This reverts commit 3d85480af32d5303f0d28e85cb80a7b3e9bf149a.

--- Assets/Scripts/Cam/FirstPersonCam.cs
+++ Assets/Scripts/Cam/FirstPersonCam.cs
@@ -20,20 +20,6 @@ public class FirstPersonCam : MonoBehaviour
 
     [SerializeField] float currentPitch = 0f;
 
-    [Header("Crouch Settings")]
-    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
-    [SerializeField] private float crouchOffset = -0.5f; // How much to lower the camera
-    [SerializeField] private float crouchSpeed = 5f;
-
-    private float defaultCameraY; // Original Y offset
-    private Vector3 targetCameraPosition;
-
-    private void Start()
-    {
-        defaultCameraY = fpCamera.transform.localPosition.y;
-        targetCameraPosition = fpCamera.transform.localPosition;
-    }
-
     public float CurrentPitch
     {
         get => currentPitch;
@@ -58,7 +44,7 @@ public class FirstPersonCam : MonoBehaviour
     private void Update()
     {
         LookInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        HandleCrouch();
+
         LookUpdate();
     }
 
@@ -73,21 +59,4 @@ public class FirstPersonCam : MonoBehaviour
         // Looking left and right
         transform.Rotate(Vector3.up * input.x);
     }
-
-    private void HandleCrouch()
-    {
-        Vector3 currentPos = fpCamera.transform.localPosition;
-
-        if (Input.GetKey(crouchKey))
-        {
-            targetCameraPosition = new Vector3(currentPos.x, defaultCameraY + crouchOffset, currentPos.z);
-        }
-        else
-        {
-            targetCameraPosition = new Vector3(currentPos.x, defaultCameraY, currentPos.z);
-        }
-
-        fpCamera.transform.localPosition = Vector3.Lerp(currentPos, targetCameraPosition, Time.deltaTime * crouchSpeed);
-    }
-
 }
